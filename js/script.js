a8d7a20e60908c52991f815d77498b515ed6a1f5
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2022-11-21 14:24:16 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
+
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-"use strict"
-
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
-  })
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
+    {
+      scope: "/ICS2O-Unit5-01-JS/",
+    }
+  )
 }
 
 /**
- * This function displays an alert.
+ * This function displays the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  document.getElementById("slider-value").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
 }
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/** 
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+  document.getElementById("answer").innerHTML =
+    "The answer was, " + randomNumber + "!" + " You got it! Good job."
+  
+    // block of code to be executed if conditional is true
+  if (valueFromSlider != randomNumber) 
+    document.getElementById("answer").innerHTML = 
+    "The answer was, " + randomNumber + " !" + "Nice guess, but try again."
+  }
