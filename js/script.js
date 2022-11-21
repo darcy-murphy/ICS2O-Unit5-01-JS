fc78953fa74ff1826ba488a186f38fcf1a344144
darcy-murphy <darcy-murphy@users.noreply.github.com>
2022-11-21 19:26:33 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
@@ -9,7 +7,7 @@
 /**
  * Check servie worker.
  */
- if (navigator.serviceWorker) {
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
     "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
     {
@@ -28,16 +26,16 @@ function myButtonClicked() {
 
 const randomNumber = Math.floor(Math.random() * 6) + 1
 
-/** 
+/**
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
   document.getElementById("answer").innerHTML =
     "The answer was, " + randomNumber + "!" + " You got it! Good job."
-  
-    // block of code to be executed if conditional is true
-  if (valueFromSlider != randomNumber) 
-    document.getElementById("answer").innerHTML = 
-    "The answer was, " + randomNumber + " !" + "Nice guess, but try again."
-  }
+
+  // block of code to be executed if conditional is true
+  if (valueFromSlider != randomNumber)
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + " !" + "Nice guess, but try again."
+}
